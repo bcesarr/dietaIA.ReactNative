179589cdb415bbec63358937d05a2aa78d39853e
bcesarr <brunocesar.as7@gmail.com>
2025-04-08 00:00:30 -0300

    Adicionando o Header ao layout

--- app/nutrition/index.tsx
+++ app/nutrition/index.tsx
@@ -1,18 +1,61 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Pressable, ScrollView } from 'react-native';
 import { useDataStore } from '../../store/data';
+import { colors } from '@/constants/colors';
 
 export default function Nutrition() {
     const user = useDataStore(state => state.user)
     console.log(user);
 
     return (
-        <View>
-            <Text>Teste Page</Text>
+        <View style={styles.container}>
+            <View style={styles.containerHeader}>
+                <View style={styles.contentHeader}>
+                    <Text style={styles.title}>Minha dieta</Text>
+                    
+                    <Pressable style={styles.buttonShare}>
+                        <Text style={styles.buttonShareText}>Compartilhar</Text>
+                    </Pressable>
+                </View>
+            </View>
         </View>
     );
 }
 
 // Criando grupo de estilos
 const styles = StyleSheet.create({
-
+    container: {
+        flex: 1,
+        backgroundColor: colors.background,
+    },
+    containerHeader: {
+        backgroundColor: colors.white,
+        borderBottomLeftRadius: 14,
+        borderBottomRightRadius: 14,
+        paddingTop: 60,
+        paddingBottom: 20,
+        marginBottom: 16
+    },
+    contentHeader: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        paddingLeft: 16,
+        paddingRight: 16
+    },
+    title: {
+        fontSize: 28,
+        fontWeight: 'bold',
+        color: colors.background
+    },
+    buttonShare: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 8,
+        backgroundColor: colors.blue,
+        borderRadius: 4
+    },
+    buttonShareText: {
+        fontWeight: 'semibold',
+        color: colors.white
+    }
 }) 
