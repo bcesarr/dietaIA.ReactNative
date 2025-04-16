d5e74b0227c68b4c3f8b1dc6d838d7f042ff7c84
bcesarr <brunocesar.as7@gmail.com>
2025-04-15 23:41:55 -0300

    finalizando o app

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,38 +1,43 @@
 import { Stack } from "expo-router";
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
 
 export default function RootLayout() {
+  const queryClient = new QueryClient();
+
   return ( 
-  <Stack> 
-    <Stack.Screen
-      name="index"
-      options={{
-        // Para tirar o Header do app
-        headerShown: false
-      }}
-    />
-    <Stack.Screen
-      name="step/index"
-      options={{
-        // Para tirar o Header do app
-        headerShown: false
-      }}
-    />
+  <QueryClientProvider client={queryClient}>
+    <Stack> 
+      <Stack.Screen
+        name="index"
+        options={{
+          // Para tirar o Header do app
+          headerShown: false
+        }}
+      />
+      <Stack.Screen
+        name="step/index"
+        options={{
+          // Para tirar o Header do app
+          headerShown: false
+        }}
+      />
 
-    <Stack.Screen
-      name="create/index"
-      options={{
-        // Para tirar o Header do app
-        headerShown: false
-      }}
-    />
+      <Stack.Screen
+        name="create/index"
+        options={{
+          // Para tirar o Header do app
+          headerShown: false
+        }}
+      />
 
-    <Stack.Screen
-      name="nutrition/index"
-      options={{
-        // Para tirar o Header do app
-        headerShown: false
-      }}
-    />
-  </Stack>
+      <Stack.Screen
+        name="nutrition/index"
+        options={{
+          // Para tirar o Header do app
+          headerShown: false
+        }}
+      />
+    </Stack>
+  </QueryClientProvider>
   );
 }
--- mobile/app/nutrition/index.tsx
+++ mobile/app/nutrition/index.tsx
@@ -1,13 +1,88 @@
-import { View, Text, StyleSheet, Pressable, ScrollView } from 'react-native';
+import { View, Text, StyleSheet, Pressable, ScrollView, Share } from 'react-native';
 import { useDataStore } from '../../store/data';
+import { api } from '../../services/api';
+import { useQueries, useQuery } from '@tanstack/react-query';
 import { colors } from '@/constants/colors';
+import { Data } from '../../types/data';
+import { Link, router } from 'expo-router';
 import { object } from 'zod';
-
 import { Feather, Ionicons } from '@expo/vector-icons';
+
+interface ResponseData {
+    data: Data
+}
 
 export default function Nutrition() {
     const user = useDataStore(state => state.user)
-    console.log(user);
+    
+    const { data, isFetching, error } = useQuery({
+        queryKey: ['nutrition'],
+        queryFn: async () => {
+            try {
+                if(!user){
+                    throw new Error('filed load nutrition')
+                }
+
+                // Para usar o teste da API ao invés da de baixo que é a principal direto na IA
+                // const response = await api.get<ResponseData>('/teste')
+
+                // Usando a API direto
+                const response = await api.post<ResponseData>('/create', {
+                    name: user.name,
+                    age: user.age,
+                    weight: user.weight,
+                    height: user.height,
+                    gender: user.gender,
+                    level: user.level,
+                    objective: user.objective
+                })
+
+                return response.data.data
+
+            } catch (err) {
+                console.log(err)
+            }
+        }
+    })
+
+    async function handleShare() {
+        try {
+            if(data && Object.keys(data).length === 0) return
+
+            const suplements = `${data?.suplementos.map( (item: string) => `${item}`)}`
+
+            const foods = `${data?.refeicoes.map( item => `\n- Nome: ${item.nome}\n- Horario: ${item.horario}\n- Alimentos: ${item.alimentos.map( alimento => `${alimento}` )}`)}`
+
+            const message = `Dieta do(a): ${data?.nome} - Objetivo: ${data?.objetivo}\n\n${foods}\n\n- Dica de Suplementos: ${suplements}`
+
+            await Share.share({
+                message: message
+            })
+
+        } catch(err) {
+            console.log(err)
+        }
+    }
+
+    if(isFetching) {
+        return (
+            <View style={styles.loading}>
+                <Text style={styles.loadingText}>Estamos gerando sua dieta!</Text>
+                <Text style={styles.loadingText}>Consultando IA...</Text>
+            </View>
+        )
+    }
+
+    if(error) {
+        return (
+            <View style={styles.loading}>
+                <Text style={styles.loadingText}>Falha ao gerar a dieta</Text>
+                <Link href="/">
+                    <Text style={styles.loadingText}>Tente novamente</Text>
+                </Link>
+            </View>
+        )
+    }
 
     return (
         <View style={styles.container}>
@@ -15,37 +90,54 @@ export default function Nutrition() {
                 <View style={styles.contentHeader}>
                     <Text style={styles.title}>Minha dieta</Text>
 
-                    <Pressable style={styles.buttonShare}>
+                    <Pressable style={styles.buttonShare} onPress={handleShare}>
                         <Text style={styles.buttonShareText}>Compartilhar</Text>
                     </Pressable>
                 </View>
             </View>
 
-            <View style={{ paddingLeft: 16, paddingRight: 16 }}>
+            <View style={{ paddingLeft: 16, paddingRight: 16, flex: 1}}>
                 {data && Object.keys(data).length > 0 && (
                     <>
                         <Text style={styles.name}>Nome: {data.nome}</Text>
-                        <Text style={styles.objective}>Foco:{data.objective}</Text>
+                        <Text style={styles.objective}>Foco: {data.objetivo}</Text>
 
                         <Text style={styles.label}>Refeições: </Text>
 
                         <ScrollView>
                             <View style={styles.foods}>
-                                {data.refeicoes.map((refeicao) => (
-                                    <View style={styles.food}>
+                                {data.refeicoes.map( (refeicao: { horario: string; nome: string; alimentos: string[] } ) => (
+                                    <View key={refeicao.nome} style={styles.food}>
                                         <View style={styles.foodHeader}>
-                                            <Text>{refeicao.nome}</Text>
+                                            <Text style={styles.foodName}>{refeicao.nome}</Text>
                                             <Ionicons name='restaurant' size={16} color={'#000'} />
                                         </View>
 
                                         <View style={styles.foodContent}>
                                             <Feather name='clock' size={14} color='#000'/>
-                                            <Text>{refeicao.horario}</Text>
-
+                                            <Text>Horario: {refeicao.horario}</Text>
                                         </View>
+
+                                        <Text style={styles.foodText}>Alimentos: </Text>
+                                        {refeicao.alimentos.map( (alimento: string) => (
+                                            <Text key={alimento}>{alimento}</Text>
+                                        ))}
+
                                     </View>
                                 ))}
                             </View>
+
+                            <View style={styles.suplements}>
+                                <Text style={styles.foodName}>Dica de suplementos: </Text>
+                                {data.suplementos.map( (item: string) => (
+                                    <Text key={item}>{item}</Text>
+                                ))}        
+                            </View>
+
+                            <Pressable style={styles.button} onPress={ () => router.replace("/") }>
+                                <Text style={styles.buttonText}>Gerar nova dieta</Text>
+                            </Pressable>
+
                         </ScrollView>
                     </>
                 )}
@@ -56,6 +148,19 @@ export default function Nutrition() {
 
 // Criando grupo de estilos
 const styles = StyleSheet.create({
+    loading: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: colors.background
+    },
+    loadingText: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 10,
+        color: colors.white,
+        marginBottom: 4
+    },
     container: {
         flex: 1,
         backgroundColor: colors.background,
@@ -104,6 +209,59 @@ const styles = StyleSheet.create({
     label: {
         fontSize: 16,
         fontWeight: 'bold',
-        color: colors.white
+        color: colors.white,
+        marginBottom: 6
     },
-}) 
+    foods: {
+        backgroundColor: colors.white,
+        borderRadius: 8,
+        marginTop: 4,
+        padding: 14,
+        gap: 8
+    },
+    food: {
+        backgroundColor: 'rgba(208, 208, 208, 0.40)',
+        borderRadius: 8,
+        padding: 8
+    },
+    foodHeader: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        marginBottom: 4
+    },
+    foodName: {
+        fontSize: 16,
+        fontWeight: 'bold',
+    },
+    foodContent: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        gap: 4
+    },
+    foodText: {
+        fontSize: 16,
+        marginBottom: 4,
+        marginTop: 14
+    },
+    suplements: {
+        backgroundColor: colors.white,
+        borderRadius: 8,
+        marginTop: 14,
+        marginBottom: 14,
+        padding: 14
+    },
+    button: {
+        height: 40,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: colors.blue,
+        borderRadius: 4,
+        marginBottom: 24
+    },
+    buttonText: {
+        fontSize: 16,
+        fontWeight: 'bold',
+        color: colors.white
+    }
+})
